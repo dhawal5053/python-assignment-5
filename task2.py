7833c318f351e1f5a00c2d34804258ad10998a39
Dhawal <dhawal345@gmail.com>
2025-07-06 12:31:12 +0530

    Added task1 and task2

---
+++ task2.py
@@ -0,0 +1,7 @@
+num = [1,2,3,4,5,6,7,8,9,10]
+first_five = num[0:5]
+
+print('Original list:',num)
+print('Extracted first five elements:',first_five)
+first_five.reverse()
+print('Reversed extracted elements',first_five)
